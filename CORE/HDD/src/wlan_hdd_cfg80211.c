e474caf4db9873541a5f8df2064af0e5fcfb4822
Gopichand Nakkala <gnakkala@qca.qualcomm.com>
2013-04-10 11:36:29 +0530

    wlan: Issue with debug prints in multiple modules.
    In many files there were missing variables and debug prints and
    also wrong format specifiers which was leading to compiler
    warnings and errors. Hence fixed all those errors and warnings.
    
    CRs-fixed: 467528
    Change-Id: Iabaabcd62654a3fef691d080f4aac2dc0e084afd

--- CORE/HDD/src/wlan_hdd_cfg80211.c
+++ CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -1794,7 +1794,7 @@ static int wlan_hdd_cfg80211_start_bss(hdd_adapter_t *pHostapdAdapter,
             pConfig->mcRSNEncryptType = mcRSNEncryptType;
             (WLAN_HDD_GET_AP_CTX_PTR(pHostapdAdapter))->ucEncryptType
                                                               = RSNEncryptType;
-            hddLog( LOG1, FL("%s: CSR AuthType = %d, "
+            hddLog( LOG1, FL("CSR AuthType = %d, "
                         "EncryptionType = %d mcEncryptionType = %d\n"),
                         RSNAuthType, RSNEncryptType, mcRSNEncryptType);
         }
@@ -1833,7 +1833,7 @@ static int wlan_hdd_cfg80211_start_bss(hdd_adapter_t *pHostapdAdapter,
                 pConfig->mcRSNEncryptType = mcRSNEncryptType;
                 (WLAN_HDD_GET_AP_CTX_PTR(pHostapdAdapter))->ucEncryptType
                                                               = RSNEncryptType;
-                hddLog( LOG1, FL("%s: CSR AuthType = %d, "
+                hddLog( LOG1, FL("CSR AuthType = %d, "
                                 "EncryptionType = %d mcEncryptionType = %d\n"),
                                 RSNAuthType, RSNEncryptType, mcRSNEncryptType);
             }
