7e316f5b4f2fb985c7ead535afd965819bb58894
sireesha kosuri <jmng34@motorola.com>
2013-05-21 18:05:35 +0530

    Merge Moto CR changes to 8953 wlan driver
    (CR): Add flag to Enable/Disable MCC mode
    (CR) : Disconnect the client device when not responded for 4 mins
    (CR) - LFR roaming instrumentation
    (CR) build: don't include debug symbols in published binary
    (CR) fix uninitialized variable.
    (CR) make function prototype match instantiation
    (CR) : Implement new IPV6 filter logic
    (CR):Enable changing wlan driver log level dynamically from applications
    (CR): Incorrect empty scan
    (CR) Hotspot MTU changes
    (CR):wlan: Destroy wakelocks in hdd_driver_init for failure cases.
    
    hdd_driver_init is called by module_init. For return failure cases, we need
     to destroy the wakelocks, otherwise it will leave non-existing wlan wakelocks in the
     kenrel wakelock list and leads kernel panic.
    
    (CR) :changed  Local_module_tags for pronto_wlan.ko module to optional for getting build in both user and userdebug builds
    
    (CR): PNO scan intervals taking default value
    
    New driver starts PNO via nl message. So change the new function
    for PNO scan intervals
    
    (CR) : WLAN: Add interface specific filter configuration
    
    Fire v6 filter configuration set onl if the adpater mode is
    INFRA on assoc and multicast listener triggers
    
    (CR) : sub ioctls for WLAN_PRIV_SET_INT_GET_NONE are not functional
    
    The PRIV IOCTL requires copy_from_user
    QC SR : 01473110
    
    (CR) : Excessive wlan logging to kernel log on by default
    
    Added code to remove unnecessary registration of filtering function
    at phone suspend and deregestaring at resume. Registration for Offloading
     filtering) to firmware is done when WiFi gets connected to an AP.
    
    (CR): Use max 10 filter slots
    Change-Id: Ibca19b339ff283732c73a9fd8b682d6ba3b07bc1
    Reviewed-on: http://gerrit.mot.com/838716
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Prashanth Chakravarthy <a19110@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- CORE/HDD/src/wlan_hdd_main.c
+++ CORE/HDD/src/wlan_hdd_main.c
@@ -139,6 +139,9 @@ int wlan_hdd_ftm_start(hdd_context_t *pAdapter);
 #define MEMORY_DEBUG_STR ""
 #endif
 #define MAX_WAIT_FOR_ROC_COMPLETION 3
+
+#define MAX_RESTART_DRIVER_EVENT_LENGTH 30
+
 /* the Android framework expects this param even though we don't use it */
 #define BUF_LEN 20
 static char fwpath_buffer[BUF_LEN];
@@ -225,6 +228,7 @@ static vos_wake_lock_t wlan_wake_lock;
 
 /* set when SSR is needed after unload */
 static e_hdd_ssr_required isSsrRequired = HDD_SSR_NOT_REQUIRED;
+static struct wake_lock wlan_wake_lock_scan; //Mot IKHSS7-28961: Incorrect empty scan results
 
 //internal function declaration
 static VOS_STATUS wlan_hdd_framework_restart(hdd_context_t *pHddCtx);
@@ -10449,6 +10453,8 @@ static void __hdd_set_multicast_list(struct net_device *dev)
       hddLog(VOS_TRACE_LEVEL_INFO,
             "%s: allow all multicast frames", __func__);
       pAdapter->mc_addr_list.mc_cnt = 0;
+      if(pAdapter->device_mode == WLAN_HDD_INFRA_STATION)
+          wlan_hdd_update_v6_filters(pAdapter, 1); // IKJB42MAIN-1244, Motorola, a19091
    }
    else
    {
@@ -10475,6 +10481,9 @@ static void __hdd_set_multicast_list(struct net_device *dev)
                MAC_ADDR_ARRAY(pAdapter->mc_addr_list.addr[i]));
          i++;
       }
+      if(pAdapter->device_mode == WLAN_HDD_INFRA_STATION)
+          wlan_hdd_update_v6_filters(pAdapter, 0); // IKJB42MAIN-1244, Motorola, a19091
+
    }
 
    if (pHddCtx->hdd_wlan_suspended)
@@ -11304,6 +11313,13 @@ void hdd_prevent_suspend_timeout(v_U32_t timeout, uint32_t reason)
                                       reason);
 
 }
+
+//Begin Mot IKHSS7-28961 : Incorrect emtpty scan results because of againg out
+void hdd_prevent_suspend_after_scan(long hz)
+{
+  wake_lock_timeout(&wlan_wake_lock_scan, hz);
+}
+//END IKHSS7-28961
 
 /**---------------------------------------------------------------------------
 
@@ -13093,6 +13109,7 @@ static int hdd_driver_init( void)
    ENTER();
 
    vos_wake_lock_init(&wlan_wake_lock, "wlan");
+   vos_wake_lock_init(&wlan_wake_lock_scan, "wlan_scan"); //Mot IKHSS7-28961: Incorrect empty scan
 
    pr_info("%s: loading driver v%s\n", WLAN_MODULE_NAME,
            QWLAN_VERSIONSTR TIMER_MANAGER_STR MEMORY_DEBUG_STR);
@@ -13114,6 +13131,7 @@ static int hdd_driver_init( void)
    if (max_retries >= 10) {
       hddLog(VOS_TRACE_LEVEL_FATAL,"%s: WCNSS driver not ready", __func__);
       vos_wake_lock_destroy(&wlan_wake_lock);
+      vos_wake_lock_destroy(&wlan_wake_lock_scan);
 #ifdef WLAN_LOGGING_SOCK_SVC_ENABLE
       wlan_logging_sock_deinit_svc();
 #endif
@@ -13186,6 +13204,7 @@ static int hdd_driver_init( void)
       vos_mem_exit();
 #endif
       vos_wake_lock_destroy(&wlan_wake_lock);
+      vos_wake_lock_destroy(&wlan_wake_lock_scan); //Mot IKHSS7-28961: Incorrect empty scan results
 #ifdef WLAN_LOGGING_SOCK_SVC_ENABLE
       wlan_logging_sock_deinit_svc();
 #endif
@@ -13346,6 +13365,7 @@ static void hdd_driver_exit(void)
 
 done:
    vos_wake_lock_destroy(&wlan_wake_lock);
+   vos_wake_lock_destroy(&wlan_wake_lock_scan); //Mot IKHSS7-28961: Incorrect empty scan results
 
    pr_info("%s: driver unloaded\n", WLAN_MODULE_NAME);
 }
@@ -13664,6 +13684,27 @@ wlan_hdd_is_GO_power_collapse_allowed (hdd_context_t* pHddCtx)
           return FALSE;
 
 }
+
+// BEGIN MOTOROLA IKJB42MAIN-274, dpn473, 01/02/2013, Add flag to disable/enable MCC mode
+v_U8_t hdd_get_mcc_mode( void )
+{
+    v_CONTEXT_t pVosContext = vos_get_global_context( VOS_MODULE_ID_HDD, NULL );
+    hdd_context_t *pHddCtx;
+
+    if (NULL != pVosContext)
+    {
+        pHddCtx = vos_get_context( VOS_MODULE_ID_HDD, pVosContext);
+        if (NULL != pHddCtx)
+        {
+            return (v_U8_t)pHddCtx->cfg_ini->enableMCC;
+        }
+    }
+    /* we are in an invalid state :( */
+    hddLog(LOGE, "%s: Invalid context", __func__);
+    return (v_U8_t)0;
+}
+// END IKJB42MAIN-274
+
 /* Decide whether to allow/not the apps power collapse. 
  * Allow apps power collapse if we are in connected state.
  * if not, allow only if we are in IMPS  */
