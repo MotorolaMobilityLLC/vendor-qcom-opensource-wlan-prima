c5d24fb4b2861a52f65a709c2eb26686323ac2c0
Bansidhar Gopalachari <bansid@codeaurora.org>
2013-06-14 20:28:46 -0700

    wlan: Revision 3.2.4.109B
    Revision 3.2.4.109B

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -59,10 +59,10 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MAJOR            3
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            4
-#define QWLAN_VERSION_EXTRA            ""
+#define QWLAN_VERSION_EXTRA            "B"
 #define QWLAN_VERSION_BUILD            109
 
-#define QWLAN_VERSIONSTR               "3.2.4.109"
+#define QWLAN_VERSIONSTR               "3.2.4.109B"
 
 #endif /* QWLAN_VERSION_H */
 
