75c1aacd0110ca93c8e6573ac6af414e1518407c
Bansidhar Gopalachari <bansid@codeaurora.org>
2013-07-15 15:56:24 -0700

    wlan: Support for NProbes, Home away time and Reassoc
    This change has support for NProbes, Scan home away
    time and HDD/SME level change for reassoc command
    
    Change-Id: I0c050b4e67ca806b405c68c04d6b99e951dc4c5f
    CRs-Fixed: 481935
    
    Conflicts:
    	CORE/HDD/src/wlan_hdd_cfg.c
    	CORE/HDD/src/wlan_hdd_main.c
    	CORE/SME/inc/csrInternal.h
    	CORE/SME/src/csr/csrApiRoam.c
    	CORE/WDI/CP/inc/wlan_qct_wdi_i.h

--- CORE/SME/inc/csrInternal.h
+++ CORE/SME/inc/csrInternal.h
@@ -620,6 +620,8 @@ typedef struct tagCsrConfig
     tANI_BOOLEAN  isWESModeEnabled;
     tANI_BOOLEAN  nRoamScanControl;
     tANI_BOOLEAN  nRoamIntraBand;
+    tANI_U8       nProbes;
+    tANI_U16      nRoamScanHomeAwayTime;
 #endif
 
 #ifdef WLAN_FEATURE_NEIGHBOR_ROAMING
