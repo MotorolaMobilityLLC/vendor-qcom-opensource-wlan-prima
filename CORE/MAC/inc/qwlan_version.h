cf71056d078bd1a172e6c5356804850ac7440c00
pratik Bhalgat <pratik_bit@codeaurora.org>
2013-09-12 13:24:12 +0530

    Wlan : Driver Version 3.2.6.109
    Wlan : Driver Version 3.2.6.109
    
    Change-Id: I3b80a3d08fed4075b27e8480defbcf97b4c76c69

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -60,9 +60,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            6
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            108
+#define QWLAN_VERSION_BUILD            109
 
-#define QWLAN_VERSIONSTR              "3.2.6.108"
+#define QWLAN_VERSIONSTR              "3.2.6.109"
 
 #endif /* QWLAN_VERSION_H */
 
