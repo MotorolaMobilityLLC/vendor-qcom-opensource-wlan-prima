ed72f6c7da6d4b7663c5caa4ac854806fabc02ba
Tushnim Bhattacharyya <tushnimb@qca.qualcomm.com>
2014-01-30 11:56:44 -0800

    wlan: UMAC: Log the packet information in kmsg.
    Printing the packet information for tx/rx mgmt, action, EAPOL
    and DHCP for debugging the connection issues.
    A new ini variable is "gEnableDebugLog" added to
    enable/disable these logs. By default this flag is disabled.
    Also added an iwpriv command "setDbgLvl" to achieve the same.
    
    Change-Id: I376fb3ed2a05744760d6563410e5eeb068b6d070
    CRs-Fixed: 600142

--- CORE/SME/src/sme_common/sme_Api.c
+++ CORE/SME/src/sme_common/sme_Api.c
@@ -1409,6 +1409,7 @@ eHalStatus sme_UpdateConfig(tHalHandle hHal, tpSmeConfigParams pSmeConfigParams)
                    "Could not pass on WNI_CFG_SCAN_IN_POWERSAVE to CCM");
        }
    }
+   pMac->fEnableDebugLog = pSmeConfigParams->fEnableDebugLog;
    return status;
 }
 
@@ -10261,5 +10262,13 @@ void activeListCmdTimeoutHandle(void *userData)
         "%s: Active List command timeout Cmd List Count %d", __func__,
         csrLLCount(&((tpAniSirGlobal) userData)->sme.smeCmdActiveList) );
     smeGetCommandQStatus((tHalHandle) userData);
+}
+
+eHalStatus sme_UpdateConnectDebug(tHalHandle hHal, tANI_U32 set_value)
+{
+    eHalStatus status = eHAL_STATUS_SUCCESS;
+    tpAniSirGlobal pMac = PMAC_STRUCT(hHal);
+    pMac->fEnableDebugLog = set_value;
+    return (status);
 }
 
