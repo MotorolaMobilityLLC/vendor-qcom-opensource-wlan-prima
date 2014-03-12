937b8fb6a6fd20468417d0470c5148bfa08b5b74
Santhosh Gillella <gvx468@motorola.com>
2014-03-12 18:58:43 -0500

    sub ioctls for WLAN_PRIV_SET_INT_GET_NONE are not functional
    The PRIV IOCTL requires copy_from_user
    QC SR : 01473110
    
    Change-Id: I99b9396eea181c49d7f44cb928b20f8e2bed8bb0
    Reviewed-on: http://gerrit.pcs.mot.com/617605
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Binesh Balasingh <binesh@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- CORE/HDD/src/wlan_hdd_wext.c
+++ CORE/HDD/src/wlan_hdd_wext.c
@@ -3942,9 +3942,10 @@ static int iw_setint_getnone(struct net_device *dev, struct iw_request_info *inf
     hdd_adapter_t *pAdapter = WLAN_HDD_GET_PRIV_PTR(dev);
     tHalHandle hHal = WLAN_HDD_GET_HAL_CTX(pAdapter);
     hdd_wext_state_t  *pWextState =  WLAN_HDD_GET_WEXT_STATE_PTR(pAdapter);
-    int *value = (int *)extra;
-    int sub_cmd = value[0];
-    int set_value = value[1];
+    int cmd_len = wrqu->data.length;
+    int *value = (int *) kmalloc(cmd_len+1, GFP_KERNEL);
+    int sub_cmd;
+    int set_value;
     int ret = 0; /* success */
     int enable_pbm, enable_mp;
 #ifdef CONFIG_HAS_EARLYSUSPEND
@@ -3958,6 +3959,18 @@ static int iw_setint_getnone(struct net_device *dev, struct iw_request_info *inf
                                   "%s:LOGP in Progress. Ignore!!!", __func__);
         return -EBUSY;
     }
+    if(value == NULL)
+       return -ENOMEM;
+
+    if(copy_from_user((char *) value, (char*)(wrqu->data.pointer), cmd_len)) {
+            hddLog(VOS_TRACE_LEVEL_FATAL, "%s -- copy_from_user --data pointer failed! bailing",
+                 __FUNCTION__);
+         kfree(value);
+         return -EFAULT;
+     }
+     sub_cmd = value[0];
+     set_value = value[1];
+     kfree(value);
 
     switch(sub_cmd)
     {
