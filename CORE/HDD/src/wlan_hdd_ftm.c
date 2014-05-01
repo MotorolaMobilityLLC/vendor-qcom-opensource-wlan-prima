653cf80d8bc98e7bc1a2e10e23e82dbc046ed27a
Santhosh Gillella <gvx468@motorola.com>
2014-05-01 16:43:30 -0500

    Revert "prima : Handling race condition in driver unload in FTM"
    This reverts commit bb07158c86d380fd304207034a61265c2fd4e528.
    
    Change-Id: I823f12f2dfb692e719cb4f7eb9d443b8c5b70ad4
    Reviewed-on: http://gerrit.mot.com/632479
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Binesh Balasingh <binesh@motorola.com>
    SLTApproved: Binesh Balasingh <binesh@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- CORE/HDD/src/wlan_hdd_ftm.c
+++ CORE/HDD/src/wlan_hdd_ftm.c
@@ -1421,15 +1421,6 @@ int wlan_hdd_ftm_close(hdd_context_t *pHddCtx)
         return VOS_STATUS_E_NOMEM;
     }
 
-    /*release the wlan_hdd_process_ftm_cmd(), if waiting for any response.*/
-    if (pHddCtx->ftm.IsCmdPending == TRUE)
-    {
-        if (vos_event_set(&pHddCtx->ftm.ftm_vos_event)!= VOS_STATUS_SUCCESS)
-        {
-            VOS_TRACE(VOS_MODULE_ID_VOSS, VOS_TRACE_LEVEL_ERROR,
-                                      "%s: vos_event_set failed", __func__);
-        }
-    }
     if(WLAN_FTM_STARTED == pHddCtx->ftm.ftm_state)
     {
         VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_FATAL,
@@ -3091,15 +3082,6 @@ void wlan_hdd_process_ftm_cmd
         hddLog(VOS_TRACE_LEVEL_ERROR,"%s: request buffer is null\n",__func__);
         return ;
     }
-
-    if (vos_is_load_unload_in_progress(VOS_MODULE_ID_HDD, NULL))
-    {
-        VOS_TRACE(VOS_MODULE_ID_VOSS, VOS_TRACE_LEVEL_WARN,
-        "%s: Load/Unload in Progress. Ignoring FTM Command %d"
-        , __func__, pRequestBuf->ftmpkt.ftm_cmd_type);
-        return ;
-    }
-
     /*Save the received request*/
     pHddCtx->ftm.pRequestBuf = pRequestBuf;
 
@@ -3220,6 +3202,7 @@ void wlan_hdd_process_ftm_cmd
 
         //HEXDUMP("Request:",(char*)pftm_data,cmd_len);
 
+        pHddCtx->ftm.IsCmdPending = TRUE;
 
         /*Post the command to the HAL*/
         if (wlan_ftm_postmsg(pftm_data, cmd_len) != VOS_STATUS_SUCCESS) {
@@ -3228,30 +3211,12 @@ void wlan_hdd_process_ftm_cmd
             return;
 
         }
-        /*After successful posting of message the command should be pending*/
-        pHddCtx->ftm.IsCmdPending = TRUE;
-
         /*Wait here until you get the response from HAL*/
         if (vos_wait_single_event(&pHddCtx->ftm.ftm_vos_event, FTM_VOS_EVENT_WAIT_TIME)!= VOS_STATUS_SUCCESS)
         {
-            hddLog(VOS_TRACE_LEVEL_ERROR,"%s: vos_wait_single_event failed",__func__);
-            pHddCtx->ftm.pResponseBuf->ftm_err_code = WLAN_FTM_FAILURE;
-            wlan_ftm_send_response(pHddCtx);
-            pHddCtx->ftm.IsCmdPending = FALSE;
+            hddLog(VOS_TRACE_LEVEL_ERROR,
+               "%s: vos_wait_single_event failed",__func__);
             return;
-        }
-        /*This check will handle the case where the completion is sent by
-          wlan_hdd_process_ftm_cmd() and not by the HAL*/
-        if (vos_is_load_unload_in_progress(VOS_MODULE_ID_HDD, NULL))
-        {
-            VOS_TRACE(VOS_MODULE_ID_VOSS, VOS_TRACE_LEVEL_WARN,
-            "%s: Load/Unload in Progress. Ignoring FTM Command %d"
-            , __func__, pRequestBuf->ftmpkt.ftm_cmd_type);
-
-            pHddCtx->ftm.pResponseBuf->ftm_err_code = WLAN_FTM_FAILURE;
-            wlan_ftm_send_response(pHddCtx);
-            pHddCtx->ftm.IsCmdPending = FALSE;
-            return ;
         }
 
         cmd_len = be16_to_cpu(pHddCtx->ftm.wnl->wmsg.length);
