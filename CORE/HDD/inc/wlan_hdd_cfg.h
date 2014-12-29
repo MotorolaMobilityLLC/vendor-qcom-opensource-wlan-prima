5cf763857601b60381764d0b5298734631ca926c
Naresh Jayaram <c_njayar@qti.qualcomm.com>
2014-12-29 18:32:47 +0530

    Add ini to decide on faster wlan connection while call via BT
    Add ini parameter "gBtcFastWlanConnPref" to decide on faster wlan
    connection when data call over BT is in progress. By default this
    is enabled.
    To edit this param, need to add this ini param to the ini file
    explicitly.
    
    Change-Id: I9668f1c7ecd0a095408e4710423f46a3c8938556
    CRs-Fixed: 777685
    Reviewed-on: http://gerrit.mot.com/704537
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: LieJun Tao <L.J.Tao@motorola.com>
    Reviewed-by: Thai Leav <w18918@motorola.com>
    Reviewed-by: Santhosh Kumar Gillella <santhoshg@motorola.com>

--- CORE/HDD/inc/wlan_hdd_cfg.h
+++ CORE/HDD/inc/wlan_hdd_cfg.h
@@ -2169,6 +2169,10 @@ This feature requires the dependent cfg.ini "gRoamPrefer5GHz" set to 1 */
 #define CFG_CTS2S_DURING_BTC_SCO_MIN            WNI_CFG_BTC_CTS2S_DURING_SCO_STAMIN
 #define CFG_CTS2S_DURING_BTC_SCO_MAX            WNI_CFG_BTC_CTS2S_DURING_SCO_STAMAX
 
+#define CFG_BTC_FAST_WLAN_CONN_PREF             "gBtcFastWlanConnPref"
+#define CFG_BTC_FAST_WLAN_CONN_PREF_DEFAULT     ( 1 )
+#define CFG_BTC_FAST_WLAN_CONN_PREF_MIN         ( 0 )
+#define CFG_BTC_FAST_WLAN_CONN_PREF_MAX         ( 1 )
 
 /*
  * Connection related log Enable/Disable.
@@ -2874,6 +2878,7 @@ typedef struct
    v_U8_t                      acsScanBandPreference;
    v_U16_t                     acsBandSwitchThreshold;
    v_U32_t                     enableDynamicRAStartRate;
+   v_BOOL_t                    btcFastWlanConnPref;
 } hdd_config_t;
 /*--------------------------------------------------------------------------- 
   Function declarations and documenation
