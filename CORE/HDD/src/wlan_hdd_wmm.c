7e9e9f24227fb1ac1ec8e090591ca60a18b5b039
Mihir Shete <smihir@qti.qualcomm.com>
2015-05-24 17:54:29 +0530

    final change to address data stall
    The patch does the following:
    1. Send ARP packets using TID 3
    
    Change-Id: Idfddc98e137505a04aa52c0281fe27438ae675cc
    Reviewed-on: http://gerrit.mot.com/751091
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Binesh Balasingh <binesh@motorola.com>
    SLTApproved: Binesh Balasingh <binesh@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-on: http://gerrit.mot.com/753930

--- CORE/HDD/src/wlan_hdd_wmm.c
+++ CORE/HDD/src/wlan_hdd_wmm.c
@@ -1818,6 +1818,10 @@ v_VOID_t hdd_wmm_classify_pkt ( hdd_adapter_t* pAdapter,
           //Give the highest priority to 802.1x packet
           if (pHdr->eth_II.h_proto == htons(HDD_ETHERTYPE_802_1_X))
               tos = 0xC0;
+          else if (pHdr->eth_II.h_proto == htons(HDD_ETHERTYPE_ARP))
+          {
+              tos = 0x60;
+          }
           else
               tos = 0;
       }
