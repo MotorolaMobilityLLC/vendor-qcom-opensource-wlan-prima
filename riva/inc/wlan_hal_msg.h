3f5b0a91dcbae8d50adaf35c1ba451f6aef022bc
Sachin Ahuja <sahuja@codeaurora.org>
2015-08-28 15:46:46 +0530

    wlan: Interface file change to get rssi when disconnect happens
    Added the interface changes to get the RSSI in case of disconnect.
    
    Change-Id: I777667831c048985271ddd013ff7b9b6b747b20a
    CRs-Fixed: 898495

--- riva/inc/wlan_hal_msg.h
+++ riva/inc/wlan_hal_msg.h
@@ -567,6 +567,7 @@ typedef enum
    WLAN_HAL_FW_MEMORY_DUMP_RSP              = 310,
    WLAN_HAL_FW_LOGGING_DXE_DONE_IND         = 311,
 
+   WLAN_HAL_LOST_LINK_PARAMETERS_IND        = 312,
    WLAN_HAL_MSG_MAX = WLAN_HAL_MSG_TYPE_MAX_ENUM_SIZE
 }tHalHostMsgType;
 
@@ -8739,6 +8740,27 @@ typedef PACKED_PRE struct PACKED_POST
    tHalMsgHeader header;
    tHalDisableMonitorModeRspParams disableMonitorModeRspParams;
 }tHalDisableMonitorModeRspMsg, *tpHalDisableMonitorModeRspMsg;
+
+/*---------------------------------------------------------------------------
+  * WLAN_HAL_LOST_LINK_PARAMETERS_IND
+  *-------------------------------------------------------------------------*/
+typedef PACKED_PRE struct PACKED_POST
+{
+    tANI_U8  bssIdx;
+    tANI_U8  rssi;
+    tSirMacAddr selfMacAddr;
+    tANI_U32 linkFlCnt;
+    tANI_U32 linkFlTx;
+    tANI_U32 lastDataRate;
+    tANI_U32 rsvd1;
+    tANI_U32 rsvd2;
+}tHalLostLinkParametersIndParams, *tpHalLostLinkParametersIndParams;
+
+typedef PACKED_PRE struct PACKED_POST
+{
+    tHalMsgHeader header;
+    tHalLostLinkParametersIndParams lostLinkParameters;
+}tHalLostLinkParametersIndMsg, *tpHalLostLinkParametersIndMsg;
 
 #if defined(__ANI_COMPILER_PRAGMA_PACK_STACK)
 #pragma pack(pop)
