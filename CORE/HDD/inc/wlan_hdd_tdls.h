64fdefa2b8643601baa256ca62e578a46fb39b73
Ganesh Kondabattini <ganeshk@codeaurora.org>
2015-09-21 13:52:09 +0530

    wlan: check if tdls exists when tdls peer timer expires.
    when wlan_hdd_tdls_initiator_wait_cb gets called, the tdls peer
    might not exists as there is a chance that the peer may get deleted
    as part of disconnection.
    
    So check if tdls exists before accessing the peer in
    wlan_hdd_tdls_initiator_wait_cb.
    
    Change-Id: I90a7a765997bf3641bf2c8de2f3c9e5068e89122
    CRs-Fixed: 906303

--- CORE/HDD/inc/wlan_hdd_tdls.h
+++ CORE/HDD/inc/wlan_hdd_tdls.h
@@ -379,4 +379,11 @@ wlan_hdd_tdls_disable_offchan_and_teardown_links(hdd_context_t *pHddCtx)
 #endif
 void wlan_hdd_tdls_update_rx_pkt_cnt_n_rssi(hdd_adapter_t *pAdapter,
         u8 *mac, v_S7_t rssiAvg);
+
+
+tdlsConnInfo_t *wlan_hdd_get_conn_info(hdd_context_t *pHddCtx,
+                                       tANI_U8 idx);
+
+v_VOID_t wlan_hdd_tdls_initiator_wait_cb(v_PVOID_t userData);
+
 #endif // __HDD_TDSL_H
