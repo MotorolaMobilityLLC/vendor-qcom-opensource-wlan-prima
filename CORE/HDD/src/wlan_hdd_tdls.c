9f753b68ac40183e45b38abde3d21afcae8f4b5c
Masti, Narayanraddi <nmasti@codeaurora.org>
2015-12-24 18:22:36 +0530

    prima: DUT is not enabling QOS even TDLS peers are QOS capable
    DUT is enabling QOS based on fixed capabilities in host which
    is leading to issue of sending data frames withou QOS header
    even though peers are capable.
    Fix is to ensure that QOS will be enabled by interpreting
    WMM IE.
    
    Change-Id: I852326f0ab27ae87bb354fc3c9eea90f425e653b
    CRs-Fixed: 955872

--- CORE/HDD/src/wlan_hdd_tdls.c
+++ CORE/HDD/src/wlan_hdd_tdls.c
@@ -1480,7 +1480,8 @@ int wlan_hdd_tdls_set_peer_caps(hdd_adapter_t *pAdapter,
                                 u8 *mac,
                                 tCsrStaParams *StaParams,
                                 tANI_BOOLEAN isBufSta,
-                                tANI_BOOLEAN isOffChannelSupported)
+                                tANI_BOOLEAN isOffChannelSupported,
+                                tANI_BOOLEAN isQosWmmSta)
 {
     hddTdlsPeer_t *curr_peer;
     hdd_context_t *pHddCtx = WLAN_HDD_GET_CTX(pAdapter);
@@ -1516,7 +1517,7 @@ int wlan_hdd_tdls_set_peer_caps(hdd_adapter_t *pAdapter,
 
     curr_peer->supported_oper_classes_len =
                StaParams->supported_oper_classes_len;
-    curr_peer->qos = StaParams->capability & CAPABILITIES_QOS_OFFSET;
+    curr_peer->qos = isQosWmmSta;
 
     mutex_unlock(&pHddCtx->tdls_lock);
     return 0;
