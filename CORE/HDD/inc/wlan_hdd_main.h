005c69d3eb4b8b99ac01a6d71b9263db4ef86ece
Sravan Kumar Kairam <sgoud@codeaurora.org>
2017-11-10 16:23:10 +0530

    wlan: Retrieve sta id for only PTK key
    Currently at add key mac address is NULL for GTK key. So while
    retrieving sta id system crashes as address is NULL. In this
    change check if pairwise key and then only retrieve sta id from
    mac.
    
    Change-Id: I2f2f6e0e10cdc6eabd78ff2dc5108969403d9a27
    CRs-Fixed: 2141572
    
    Mot-CRs-fixed:(CR)
    
    Signed-off-by: vineetv <vineetv@motorola.com>
    Change-Id: I289df63dc38bb7551384cd04511e7b77daebc92d
    Reviewed-on: https://gerrit.mot.com/1109365
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: Prashanth Chakravarthy <a19110@motorola.com>
    Tested-by: Jira Key
    Submit-Approved: Jira Key
    (cherry picked from commit 5d8cd74cab74c92bc7bc5b328719010cd9cdbc09)

--- CORE/HDD/inc/wlan_hdd_main.h
+++ CORE/HDD/inc/wlan_hdd_main.h
@@ -240,6 +240,8 @@ typedef v_U8_t tWlanHddMacAddr[HDD_MAC_ADDR_LEN];
 
 #define WLAN_WAIT_TIME_EXTSCAN  1000
 
+#define HDD_MAX_STA_COUNT (HAL_NUM_STA)
+
 /*
  * Generic asynchronous request/response support
  *
