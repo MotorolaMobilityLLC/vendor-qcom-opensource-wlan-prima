01f1f42ace9b05c3075297812ef41ff617193d58
Gilberto Gambugge <gambugge@motorola.com>
2017-12-11 15:15:11 -0200

    Revert "prima: Wait for clear filter request's response from firmware"
    This reverts commit 73010b90de91d9dda417705e3d119c7184de607f.
    
    ANR and timeouts setting/clearing IPv6 filters are observed with this change.
    This code was not present in Android N.
    
    Change-Id: I3af0d1cd8f798becbd0d9ccd53ab446a5563048c
    Reviewed-on: https://gerrit.mot.com/1102685
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Bin Liu <liubin7@motorola.com>
    Reviewed-by: Ranjeet Gupta <ranjeetgupta@motorola.com>
    Submit-Approved: Jira Key

--- CORE/WDA/src/wlan_qct_wda.c
+++ CORE/WDA/src/wlan_qct_wda.c
@@ -20900,7 +20900,6 @@ void WDA_ReceiveFilterClearFilterRespCallback(
                         void * pUserData)
 {
    tWDA_ReqParams *pWdaParams = (tWDA_ReqParams *)pUserData;
-   tSirRcvFltPktClearParam *pktClearParam;
 
    VOS_TRACE( VOS_MODULE_ID_WDA, VOS_TRACE_LEVEL_INFO,
                                           "<------ %s " ,__func__);
@@ -20911,14 +20910,6 @@ void WDA_ReceiveFilterClearFilterRespCallback(
                  "%s: pWdaParams received NULL", __func__);
       VOS_ASSERT(0) ;
       return ;
-   }
-
-   pktClearParam = (tSirRcvFltPktClearParam *)pWdaParams->wdaMsgParam;
-   if(pktClearParam->pktFilterCallback)
-   {
-       pktClearParam->pktFilterCallback(
-            pktClearParam->cbCtx,
-            CONVERT_WDI2SIR_STATUS(pwdiRcvFltPktClearRspParamsType->wdiStatus));
    }
 
    vos_mem_free(pWdaParams->wdaWdiApiMsgParam);
@@ -20937,7 +20928,6 @@ void WDA_ReceiveFilterClearFilterRespCallback(
 void WDA_ReceiveFilterClearFilterReqCallback(WDI_Status wdiStatus, void* pUserData)
 {
    tWDA_ReqParams *pWdaParams = (tWDA_ReqParams *)pUserData;
-   tSirRcvFltPktClearParam *pktClearParam;
 
    VOS_TRACE( VOS_MODULE_ID_WDA, VOS_TRACE_LEVEL_INFO,
               "<------ %s, wdiStatus: %d", __func__, wdiStatus);
@@ -20952,13 +20942,6 @@ void WDA_ReceiveFilterClearFilterReqCallback(WDI_Status wdiStatus, void* pUserDa
 
    if(IS_WDI_STATUS_FAILURE(wdiStatus))
    {
-      pktClearParam = (tSirRcvFltPktClearParam *)pWdaParams->wdaMsgParam;
-      if(pktClearParam->pktFilterCallback)
-      {
-          pktClearParam->pktFilterCallback(
-              pktClearParam->cbCtx,
-              CONVERT_WDI2SIR_STATUS(wdiStatus));
-      }
       vos_mem_free(pWdaParams->wdaWdiApiMsgParam);
       vos_mem_free(pWdaParams->wdaMsgParam);
       vos_mem_free(pWdaParams);
@@ -21016,12 +20999,6 @@ VOS_STATUS WDA_ProcessReceiveFilterClearFilterReq (tWDA_CbContext *pWDA,
    {
       VOS_TRACE( VOS_MODULE_ID_WDA, VOS_TRACE_LEVEL_ERROR,
               "Failure in WDA_ProcessReceiveFilterClearFilterReq(), free all the memory " );
-      if(pRcvFltPktClearParam->pktFilterCallback)
-      {
-          pRcvFltPktClearParam->pktFilterCallback(
-                pRcvFltPktClearParam->cbCtx,
-                CONVERT_WDI2SIR_STATUS(status));
-      }
       vos_mem_free(pWdaParams->wdaWdiApiMsgParam) ;
       vos_mem_free(pWdaParams->wdaMsgParam);
       vos_mem_free(pWdaParams);
