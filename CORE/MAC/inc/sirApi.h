6b11c6e706e71637a74dbf2851c1937c65df1a51
Gilberto Gambugge <gambugge@motorola.com>
2017-12-11 15:15:11 -0200

    Revert "prima: Wait for clear filter request's response from firmware"
    This reverts commit 73010b90de91d9dda417705e3d119c7184de607f.
    
    ANR and timeouts setting/clearing IPv6 filters are observed with this change.
    This code was not present in Android N.
    
    Change-Id: I3af0d1cd8f798becbd0d9ccd53ab446a5563048c
    Reviewed-on: https://gerrit.mot.com/1102685
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Bin Liu <liubin7@motorola.com>
    Reviewed-by: Ranjeet Gupta <ranjeetgupta@motorola.com>
    Submit-Approved: Jira Key

--- CORE/MAC/inc/sirApi.h
+++ CORE/MAC/inc/sirApi.h
@@ -4342,8 +4342,6 @@ typedef struct sSirRcvFltPktClearParam
   tANI_U8    filterId;
   tSirMacAddr selfMacAddr;
   tSirMacAddr bssId;
-  pktFilterReqCb     pktFilterCallback;
-  void        *cbCtx;
 }tSirRcvFltPktClearParam, *tpSirRcvFltPktClearParam;
 
 //
