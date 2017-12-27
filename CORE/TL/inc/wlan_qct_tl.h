179a44fc8c9c8c9a7bd045cb2ec5abcecc915757
Sravan Kumar Kairam <sgoud@codeaurora.org>
2017-12-27 13:11:57 +0530

    wlan: Update key sequence counter to TL
    Currently the key sequence counter received from user space is
    not updated at TL. So update the sequence counter to TL.
    
    Change-Id: I460d64511502fd414d0e8568c69712bbe54dd7fa
    CRs-Fixed: 2136293
    
    Mot-CRs-fixed:(CR)
    
    Signed-off-by: vineetv <vineetv@motorola.com>
    Change-Id: Iaadcf3268b6cc74d16ede08eaf89195b007d4ab6
    Reviewed-on: https://gerrit.mot.com/1109359
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Prashanth Chakravarthy <a19110@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit 73e7548f35d20d736cadc1502bdbc7a99dd560df)

--- CORE/TL/inc/wlan_qct_tl.h
+++ CORE/TL/inc/wlan_qct_tl.h
@@ -3352,4 +3352,14 @@ WLANTL_SetMcastDuplicateDetection
 );
 #endif /* WLAN_FEATURE_RMC */
 
+/**
+ * WLANTL_SetKeySeqCounter() - set sequence key counter
+ * @pvosGCtx: global vos context
+ * @counter: key sequence counter
+ * @staid: station index
+ *
+ * Return: void
+ */
+void WLANTL_SetKeySeqCounter(void *pvosGCtx, u64 counter, uint8_t staid);
+
 #endif /* #ifndef WLAN_QCT_WLANTL_H */
