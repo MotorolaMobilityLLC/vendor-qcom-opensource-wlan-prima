1aebdbe961e5cbff62590665c4c404cbdae66590
sunyue5 <sunyue5@motorola.com>
2018-04-09 14:58:40 +0800

    rework "remove hotspot 5G Band1 support for European 33 countries"
    Change-Id: I3f193418a76dad6928fc99a9d9a121daaaf08a8a
    Signed-off-by: sunyue5 <sunyue5@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1160206
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhilu Yin <yinzl1@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit 87fc5db7819e0b93c88c8a8b33fae67114684582)
    Reviewed-on: https://gerrit.mot.com/1160312
    Submit-Approved: Yue Sun <sunyue5@mt.com>

--- CORE/HDD/src/wlan_hdd_hostapd.c
+++ CORE/HDD/src/wlan_hdd_hostapd.c
@@ -3829,6 +3829,7 @@ int __iw_softap_get_channel_list(struct net_device *dev,
     tpChannelListInfo channel_list = (tpChannelListInfo) extra;
     eCsrBand curBand = eCSR_BAND_ALL;
     hdd_context_t *pHddCtx;
+    tpAniSirGlobal pMac; //IKSWO-79967
     int ret = 0;
 
     ENTER();
@@ -3858,6 +3859,7 @@ int __iw_softap_get_channel_list(struct net_device *dev,
         hddLog(LOGE,FL("not able get the current frequency band"));
         return -EIO;
     }
+    pMac = PMAC_STRUCT( hHal ); //IKSWO-79967
     wrqu->data.length = sizeof(tChannelListInfo);
     ENTER();
 
@@ -3894,8 +3896,10 @@ int __iw_softap_get_channel_list(struct net_device *dev,
         hddLog(LOGE,FL("Failed to get Domain ID, %d"),domainIdCurrentSoftap);
         return -EIO;
     }
-
-    if(REGDOMAIN_FCC == domainIdCurrentSoftap &&
+    //BEGIN IKSWO-79967, check if current country need disable MHS 5G Band1
+    if( (VOS_TRUE == vos_IsDisableMhsBand1CountryCode(pMac->scan.countryCodeCurrent) ||
+             REGDOMAIN_FCC == domainIdCurrentSoftap) &&
+    //END IKSWO-79967
              pHddCtx->cfg_ini->gEnableStrictRegulatoryForFCC )
     {
         for(i = 0; i < temp_num_channels; i++)
