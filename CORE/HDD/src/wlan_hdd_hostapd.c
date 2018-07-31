5b6205850d64e58b96a5e17bfa7b75d55d84aa34
sunyue5 <sunyue5@motorola.com>
2018-07-31 18:22:07 +0800

    Disable MHS 5G Band1 for European 33 countries
    We no longer use qsap qsapsetSoftap, so that we directly limit the MHS channel range
    in driver instead of return channel list for iw_softap_get_channel_list
    
    Change-Id: Id89ee75308c398a20969fab2269fd59282f36b62
    Signed-off-by: sunyue5 <sunyue5@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1218228
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Bin Liu <liubin7@motorola.com>
    Reviewed-by: Zhilu Yin <yinzl1@motorola.com>
    Submit-Approved: Jira Key

--- CORE/HDD/src/wlan_hdd_hostapd.c
+++ CORE/HDD/src/wlan_hdd_hostapd.c
@@ -3953,7 +3953,6 @@ int __iw_softap_get_channel_list(struct net_device *dev,
     tpChannelListInfo channel_list = (tpChannelListInfo) extra;
     eCsrBand curBand = eCSR_BAND_ALL;
     hdd_context_t *pHddCtx;
-    tpAniSirGlobal pMac; //IKSWO-79967
     int ret = 0;
 
     ENTER();
@@ -3983,7 +3982,6 @@ int __iw_softap_get_channel_list(struct net_device *dev,
         hddLog(LOGE,FL("not able get the current frequency band"));
         return -EIO;
     }
-    pMac = PMAC_STRUCT( hHal ); //IKSWO-79967
     wrqu->data.length = sizeof(tChannelListInfo);
     ENTER();
 
@@ -4020,10 +4018,7 @@ int __iw_softap_get_channel_list(struct net_device *dev,
         hddLog(LOGE,FL("Failed to get Domain ID, %d"),domainIdCurrentSoftap);
         return -EIO;
     }
-    //BEGIN IKSWO-79967, check if current country need disable MHS 5G Band1
-    if( (VOS_TRUE == vos_IsDisableMhsBand1CountryCode(pMac->scan.countryCodeCurrent) ||
-             REGDOMAIN_FCC == domainIdCurrentSoftap) &&
-    //END IKSWO-79967
+    if(REGDOMAIN_FCC == domainIdCurrentSoftap &&
              pHddCtx->cfg_ini->gEnableStrictRegulatoryForFCC )
     {
         for(i = 0; i < temp_num_channels; i++)
