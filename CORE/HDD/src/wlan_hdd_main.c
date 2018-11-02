54e6b08e9573e1b724a4c0b259c84ed91600c4c1
Binesh B <binesh@motorola.com>
2018-11-01 21:33:08 -0500

    Add new sysfs entries for SAR cutback
    create sar_mhs and sar_sta sysfs entries for recording wlan states
    for SAR cutback. The WLAN driver does not care about these entries
    but they are needed as IPC over to the mdmcutback daemon
    
    Change-Id: I8241e92f8033241664eae638abcf6bef6d6dea24
    Reviewed-on: https://gerrit.mot.com/1263877
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Binesh Balasingh <binesh@motorola.com>
    Submit-Approved: Jira Key

--- CORE/HDD/src/wlan_hdd_main.c
+++ CORE/HDD/src/wlan_hdd_main.c
@@ -156,6 +156,18 @@ static char *country_code;
 static int   enable_11d = -1;
 static int   enable_dfs_chan_scan = -1;
 
+#define BUF_LEN_SAR 10
+static char  sar_sta_buffer[BUF_LEN_SAR];
+static struct kparam_string sar_sta = {
+   .string = sar_sta_buffer,
+   .maxlen = BUF_LEN_SAR,
+};
+static char  sar_mhs_buffer[BUF_LEN_SAR];
+static struct kparam_string sar_mhs = {
+   .string = sar_mhs_buffer,
+   .maxlen = BUF_LEN_SAR,
+};
+
 #ifndef MODULE
 static int wlan_hdd_inited;
 #endif
@@ -18282,6 +18294,12 @@ bool hdd_is_cli_iface_up(hdd_context_t *hdd_ctx)
 	return false;
 }
 
+static int sar_changed_handler(const char *kmessage,
+                                  const struct kernel_param *kp)
+{
+   return param_set_copystring(kmessage, kp);
+}
+
 //Register the module init/exit functions
 module_init(hdd_module_init);
 module_exit(hdd_module_exit);
@@ -18297,6 +18315,11 @@ static const struct kernel_param_ops con_mode_ops = {
 
 static const struct kernel_param_ops fwpath_ops = {
 	.set = fwpath_changed_handler,
+	.get = param_get_string,
+};
+
+static const struct kernel_param_ops sar_ops = {
+	.set = sar_changed_handler,
 	.get = param_get_string,
 };
 
@@ -18318,3 +18341,10 @@ module_param(enable_11d, int,
 
 module_param(country_code, charp,
              S_IRUSR | S_IRGRP | S_IROTH);
+
+module_param_cb(sar_sta, &sar_ops, &sar_sta,
+                    S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+module_param_cb(sar_mhs, &sar_ops, &sar_mhs,
+                    S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
